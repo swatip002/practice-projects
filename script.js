3b46ba0844753dc96e9a66e205e3b1de717d662f
swatip002 <swatiprakrutidas2002@gmail.com>
2025-12-29 10:48:25 +0530

    customized cursor

--- script.js
+++ script.js
@@ -1,45 +1,9 @@
-let arr = [
-    
-    {
-        team: 'CSK',
-        primary: 'yellow',
-        secondary: 'blue',
-    },
-    {
-        team: 'RCB',
-        primary: 'red',
-        secondary: 'black'
-    },
-    {
-        team: 'MI',
-        primary: 'blue',
-        secondary: 'gold'
-    },
-    {
-        team: 'KKR',
-        primary: 'purple',
-        secondary: 'gold'
-    },
-    {
-        team: 'SRH',
-        primary: 'orange',
-        secondary: 'black'
-    },
-    {
-        team: 'PBKS',
-        primary: 'crimson',
-        secondary: 'silver'
-    },
-
-]
-
 let main = document.querySelector('main');
-let btn = document.querySelector('button');
-let h1 = document.querySelector('h1');
+let cursor = document.querySelector('#cursor')
 
-btn.addEventListener('click',function(){
-    let winner = Math.floor(Math.random()*arr.length);
-    h1.innerHTML = arr[winner].team;
-    main.style.backgroundColor = arr[winner].secondary;
-    h1.style.backgroundColor = arr[winner].primary;
+main.addEventListener('mousemove',function(val){
+    //console.log(val);
+    cursor.style.left = val.x + 'px';
+    cursor.style.top = val.y + 'px';
+    
 })
