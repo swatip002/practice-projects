d74524b12a910a194f1a6e6d57979abfc27029ff
swatip002 <swatiprakrutidas2002@gmail.com>
2026-01-06 23:07:19 +0530

    practice10

--- script.js
+++
@@ -1,9 +0,0 @@
-let main = document.querySelector('main');
-let cursor = document.querySelector('#cursor')
-
-main.addEventListener('mousemove',function(val){
-    //console.log(val);
-    cursor.style.left = val.x + 'px';
-    cursor.style.top = val.y + 'px';
-    
-})
